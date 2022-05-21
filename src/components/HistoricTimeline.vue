8a0f9c130eec6f6cead237a6b4c42345ecf5ca25
John Gasper <jtgasper3@gmail.com>
2022-05-20 22:43:52 -0700

    Addesd search filtering

--- src/components/HistoricTimeline.vue
+++ src/components/HistoricTimeline.vue
@@ -1,40 +1,55 @@
 <template>
-  <v-timeline>
-    <v-timeline-item
-      v-for="event in filteredEvents"
-      :key="event.id"
-      fill-dot
-      :dot-color="getCategoryColor(event.category)"
-      size="small"
-    >
-      <template #opposite>
-        <div
-          :class="`pt-1 headline font-weight-bold text-${getCategoryColor(
-            event.category
-          )}`"
-        >{{ formatDate(event.date) }}</div>
-      </template>
-
-      <div>
-        <h2
-          v-if="event.title"
-          :class="`mt-n1 headline font-weight-light mb-4 text-${getCategoryColor(
-            event.category
-          )} justify-end`"
+  <v-row>
+    <v-col>
+      <v-text-field
+        v-model="state.searchString"
+        variant="contained"
+        hint="Search the timeline for specific text. (Other filters are available from the Filter menu.)"
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+      />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col>
+      <v-timeline>
+        <v-timeline-item
+          v-for="event in filteredEvents"
+          :key="event.id"
+          fill-dot
+          :dot-color="getCategoryColor(event.category)"
+          size="small"
         >
-          {{ event.title }}
-        </h2>
-        <div v-if="event.subtitle">{{
-          event.subtitle
-        }}</div>
-        <div v-if="event.description">
-          {{ event.description }}
-        </div>
-      </div>
-    </v-timeline-item>
-  </v-timeline>
-  
-  <v-dialog 
+          <template #opposite>
+            <div
+              :class="`pt-1 headline font-weight-bold text-${getCategoryColor(
+                event.category
+              )}`"
+            >
+              {{ formatDate(event.date) }}
+            </div>
+          </template>
+
+          <div>
+            <h2
+              v-if="event.title"
+              :class="`mt-n1 headline font-weight-light mb-4 text-${getCategoryColor(
+                event.category
+              )} justify-end`"
+            >
+              {{ event.title }}
+            </h2>
+            <div v-if="event.subtitle">{{ event.subtitle }}</div>
+            <div v-if="event.description">
+              {{ event.description }}
+            </div>
+          </div>
+        </v-timeline-item>
+      </v-timeline>
+    </v-col>
+  </v-row>
+
+  <v-dialog
     v-model="state.showFilters"
     fullscreen
     :scrim="false"
@@ -48,19 +63,31 @@
         top="104"
         right="-35"
         width="90"
-        color="rgba(0, 0, 0, 0.3)"
+        color="rgba(0, 0, 0, 0.1)"
         theme="dark"
       >
-        <v-btn
-          icon="mdi-filter-menu"
-          variant="plain"
-          v-bind="props"
-          
-        >
-        </v-btn>
+        <v-tooltip anchor="start">
+          <template #activator="{ props: tipProps }">
+            <v-btn
+              color="primary"
+              icon=""
+              theme="dark"
+              variant="plain"
+              v-bind="props"
+            >
+              <v-icon color="primary" dark v-bind="tipProps">
+                mdi-filter-menu
+              </v-icon>
+            </v-btn>
+          </template>
+          <span>Filter</span>
+        </v-tooltip>
       </v-card>
     </template>
-    <historic-timeline-filters :filters="state.filters" @close="state.showFilters = false"></historic-timeline-filters>
+    <historic-timeline-filters
+      :filters="state.filters"
+      @close="state.showFilters = false"
+    ></historic-timeline-filters>
   </v-dialog>
 </template>
 
@@ -92,6 +119,7 @@ const state = reactive({
     dateRangeMin: 0,
     dateRangeMax: 0,
   },
+  searchString: "",
   showFilters: false,
 });
 
@@ -100,7 +128,15 @@ onMounted(() => {
 });
 
 const filteredEvents = computed(() => {
-  return state.events
+  let searchStringPattern;
+
+  try {
+    searchStringPattern = new RegExp(state.searchString, "i");
+  } catch (e) {
+    // searchStringPattern will be falsy
+  }
+
+  const initialFiltered = state.events
     .filter((e) => state.filters.categories.includes(e.category))
     .filter((e) => state.filters.denominations.includes(e.denomination))
     .filter(
@@ -108,6 +144,16 @@ const filteredEvents = computed(() => {
         e.date > new Date(state.filters.dateRange[0], 0) &&
         e.date < new Date(state.filters.dateRange[1], 11)
     );
+  if (!searchStringPattern) {
+    return initialFiltered;
+  }
+
+  return initialFiltered.filter(
+    (e) =>
+      searchStringPattern.test(e.title) ||
+      searchStringPattern.test(e.subtitle) ||
+      searchStringPattern.test(e.description)
+  );
 });
 
 function getCategoryColor(category) {
@@ -137,7 +183,10 @@ watch(
         ) / 10
       ) * 10;
     if (state.filters.dateRange.length === 0) {
-      state.filters.dateRange = [state.filters.dateRangeMin, state.filters.dateRangeMax];
+      state.filters.dateRange = [
+        state.filters.dateRangeMin,
+        state.filters.dateRangeMax,
+      ];
     }
 
     if (state.filters.categories.length === 0) {
@@ -149,7 +198,9 @@ watch(
     if (state.filters.denominations.length === 0) {
       state.filters.denominations = state.events
         .map((e) => e.denomination)
-        .filter((denominations, index, arr) => arr.indexOf(denominations) == index);
+        .filter(
+          (denominations, index, arr) => arr.indexOf(denominations) == index
+        );
     }
   }
 );
@@ -158,4 +209,3 @@ function formatDate(date) {
   return format(date, "PPP");
 }
 </script>
-
